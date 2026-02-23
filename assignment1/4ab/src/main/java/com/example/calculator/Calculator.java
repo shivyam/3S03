eef2b26bf7f63a098b17ea04b4e88aeefb07ac1b
Shivya Mehta <shivyamehta1@gmail.com>
2026-02-23 11:15:14 -0800

    remove space

--- assignment1/4ab/src/main/java/com/example/calculator/Calculator.java
+++ assignment1/4ab/src/main/java/com/example/calculator/Calculator.java
@@ -5,7 +5,6 @@ public class Calculator {
     public double divide(double numerator, double denominator){
         if (denominator == 0){
             throw new ArithmeticException("Division by zero");
-
         }
         return numerator/denominator;
     }
